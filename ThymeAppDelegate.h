ab5500452873bad34930f15b927474e53c64ab2f
João <mail@joaomoreno.com>
2010-06-11 19:23:20 +0200

    Removed references from 10.5 from master branch.

--- ThymeAppDelegate.h
+++ ThymeAppDelegate.h
@@ -9,11 +9,7 @@
 #import "DDHotKeyCenter.h"
 #import <Growl/Growl.h>
 
-#if (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_5)
-@interface ThymeAppDelegate : NSObject <GrowlApplicationBridgeDelegate>
-#else
 @interface ThymeAppDelegate : NSObject <NSApplicationDelegate, GrowlApplicationBridgeDelegate>
-#endif
 {
     // Session
     NSInteger hours;
