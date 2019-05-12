a4054b85760982d9fa2b0ed4f14d1e7eb3cf446a
ErezVolk <ErezVolk@users.noreply.github.com>
2019-05-12 17:50:46 +0300

    Update PreferencesWindowController.m
    Fix by @smokris for `Failed to connect (appearDisabledWhilePausedButton) outlet from (PreferencesWindowController) to (NSButton)`
    
    Co-Authored-By: Steve Mokris <smokris@softpixel.com>

--- PreferencesWindowController.m
+++ PreferencesWindowController.m
@@ -43,7 +43,7 @@
     
     [self.pauseOnSleepButton bind:NSValueBinding toObject:defaults withKeyPath:@"values.pauseOnSleep" options:nil];
     [self.pauseOnScreensaverButton bind:NSValueBinding toObject:defaults withKeyPath:@"values.pauseOnScreensaver" options:nil];
-    [self.appearDisabledWhilePaused bind:NSValueBinding toObject:defaults withKeyPath:@"values.appearDisabledWhilePaused" options:nil];
+    [self.appearDisabledWhilePausedButton bind:NSValueBinding toObject:defaults withKeyPath:@"values.appearDisabledWhilePaused" options:nil];
     
     [self.startPauseShortcutRecorder clearButtonRect];
     
